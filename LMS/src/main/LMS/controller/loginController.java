aeee28fb8ed1a7ee9f4447b3b8dd1af6a307e891
Mohamed Aziz TURKI <92651066+mazizturki@users.noreply.github.com>
2024-11-29 17:02:41 +0100

    Add files via upload

---
+++ LMS/src/main/LMS/controller/loginController.java
@@ -0,0 +1,42 @@
+package com.vierund.LMS.controller;
+
+import com.vierund.LMS.entity.Membres;
+import com.vierund.LMS.service.loginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class loginController {
+
+    @Autowired
+    private loginService LogService;
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("cin") String cin,
+                        @RequestParam("mdp") String mdp, Model model) {
+        Membres utilisateur = LogService.authenticate(cin, mdp);
+
+        if (utilisateur != null) {
+            if ("M".equals(utilisateur.getStatut())) {
+                return "membreDashboard";
+            } else if ("A".equals(utilisateur.getStatut())) {
+                return "adminDashboard";
+            } else {
+                model.addAttribute("error", "Statut inconnu");
+                return "login";
+            }
+        } else {
+            model.addAttribute("error", "Identifiants incorrects");
+            return "login";
+        }
+    }
+}
---
+++ LMS/src/main/LMS/entity/admin.java
@@ -0,0 +1,15 @@
+package com.vierund.LMS.entity;
+
+public class admin extends Membres {
+
+    public admin(String nom, String prenom, String cin, String dateNaiss, String email, String mdp, String uni, String tel) {
+        super(nom, prenom, cin, dateNaiss, email, mdp, uni, tel);
+        super.setStatut("A");
+    }
+
+    @Override
+    public void setStatut(String statut) {
+        throw new UnsupportedOperationException("Le statut d'un administrateur ne peut pas être modifié.");
+    }
+}
+
